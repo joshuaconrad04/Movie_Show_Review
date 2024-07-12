321a1b458f7db043b3f6648d53967127b221bfd7
Joshua COnrad <joshuaconrad04@gmail.com>
2024-07-12 11:34:00 -0500

    added ability to view all notes from database

--- app.js
+++ app.js
@@ -12,6 +12,85 @@ app.use(express.static("public"));
 
  app.set('view engine', 'ejs');
 
+ //connecting to my pg database
+ const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "movies",
+  password: "Boomboom#1212",
+  port: 5432,
+});
+db.connect();
+
+//to create a new post
+app.get("/new", (req, res) => {
+  res.render("newpost.ejs", { heading: "New Post", submit: "Create Post" });
+});
+
+
+//To edit a post
+app.get("/edit/:id", (req, res) => {
+  const ID =req.params.id;
+ 
+
+
+});
+
+// Create a new post
+app.post("/api/posts", async (req, res) => {
+  try {
+    const response = await axios.post(`${API_URL}/posts`, req.body);
+    console.log(response.data);
+    res.redirect("/");
+  } catch (error) {
+    res.status(500).json({ message: "Error creating post" });
+  }
+});
+
+// Partially update a post
+app.post("/api/posts/:id", async (req, res) => {
+  console.log("called");
+  try {
+    const response = await axios.patch(
+      `${API_URL}/posts/${req.params.id}`,
+      req.body
+    );
+    console.log(response.data);
+    res.redirect("/");
+  } catch (error) {
+    res.status(500).json({ message: "Error updating post" });
+  }
+});
+
+// Delete a post
+app.get("/delete/:id",  (req, res) => {
+  try {
+   
+  } catch (error) {
+    
+  }
+});
+
+
+
+
+
+
+app.get('/', async(req, res) => {
+
+  try {
+    const result = (await db.query("SELECT * FROM product_reviews ORDER BY review_id ASC")).rows;
+    const user=(await db.query("SELECT * FROM users ORDER BY id ASC")).rows;
+    res.render("index", {
+      title:'home',
+      posts: result,
+      users:user
+    });
+  } catch (err) {
+    console.log(err);
+  }
+
+});
 
 app.get('/reviews', (req, res) => {
   res.render('index', { title: 'Home' });
